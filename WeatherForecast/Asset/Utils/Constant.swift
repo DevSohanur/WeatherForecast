c23a9c0680a3dd8b6562b65aacd2b2107ec84442
Sohanur <39675963+fr2sr@users.noreply.github.com>
2023-06-05 12:07:26 +0600

    Open Weather API Key Changed
    Due to exceed of daily limit, I need to change the API key

--- WeatherForecast/Asset/Utils/Constant.swift
+++ WeatherForecast/Asset/Utils/Constant.swift
@@ -9,7 +9,8 @@ import Foundation
 
 class Constant{
     
-    static let OPEN_WEATHER_API_KEY: String = "0d684dd73f98027229156f36f2b75da4"
+//    static let OPEN_WEATHER_API_KEY: String = "0d684dd73f98027229156f36f2b75da4"
+    static let OPEN_WEATHER_API_KEY: String = "f997433ea9afaaf321a42960718d3c44"
     
     static let FORECAST_WEATHER_API_URL: String = "https://api.openweathermap.org/data/2.5/forecast"
     static let CURRENT_WEATHER_API_URL: String = "https://api.openweathermap.org/data/2.5/weather"
